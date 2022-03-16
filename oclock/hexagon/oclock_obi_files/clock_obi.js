d2d8798a750169dfcc770fea93059b170f5572b9
wdingsoft <wdingbox@gmail.com>
2022-03-16 10:49:48 +0800

    new file:   ../hexagon/Hclock.html transform.htm new file:   ../hexagon/oclock_obi_files/62607.gif transform.htm new file:   ../hexagon/oclock_obi_files/62613.gif transform.htm new file:   ../hexagon/oclock_obi_files/62619.gif transform.htm new file:   ../hexagon/oclock_obi_files/62620.gif transform.htm new file:   ../hexagon/oclock_obi_files/62624.gif transform.htm new file:   ../hexagon/oclock_obi_files/62629.gif transform.htm new file:   ../hexagon/oclock_obi_files/62633.gif transform.htm new file:   ../hexagon/oclock_obi_files/62636.gif transform.htm new file:   ../hexagon/oclock_obi_files/62643.gif transform.htm new file:   ../hexagon/oclock_obi_files/62652.gif transform.htm new file:   ../hexagon/oclock_obi_files/62656.gif transform.htm new file:   ../hexagon/oclock_obi_files/62662.gif transform.htm new file:   ../hexagon/oclock_obi_files/clock.js transform.htm new file:   ../hexagon/oclock_obi_files/clock_obi.js transform.htm new file:   ../hexagon/oclock_obi_files/clock_step0_background.css transform.htm new file:   ../hexagon/oclock_obi_files/clock_step1_clock.css transform.htm new file:   ../hexagon/oclock_obi_files/clock_step2_outer_clock_face.css transform.htm new file:   ../hexagon/oclock_obi_files/clock_step2b_making.css transform.htm new file:   ../hexagon/oclock_obi_files/clock_step2c_inner_clock.css transform.htm new file:   ../hexagon/oclock_obi_files/clock_step3_hands.css transform.htm new file:   ../hexagon/oclock_obi_files/clock_step3_hexagon.css transform.htm new file:   ../hexagon/oclock_obi_files/obi.css transform.htm new file:   transform.htm

---
+++ oclock/hexagon/oclock_obi_files/clock_obi.js
@@ -0,0 +1,35 @@
+
+function init_obimg() {
+    var clar = document.getElementsByClassName("obimg")
+    var offx = 12.5, offy = 12.5
+    for (var i = 0; i < clar.length; i++) {
+        var iR = 9
+        if (i < 6) iR = 11.5
+        var clk = clar[i]
+        var x = offx + iR * Math.cos(i * Math.PI / 3 - Math.PI / 2)
+        var y = offy + iR * Math.sin(i * Math.PI / 3 - Math.PI / 2)
+        console.log(x, y)
+        clk.style.transform = `translate(${x}rem,${y}rem)`
+        clk.style.opacity = 0.3 + 0.2 * (Math.sin(i * Math.PI / 6 - Math.PI / 2));//(i+1)/15/2
+        clk.style.border = "2px solid #000000"
+
+
+        if (isNowObi(i)) {
+            var ring = document.getElementsByClassName("obimgRing")
+            ring[0].style.transform = `translate(${x}rem,${y}rem)`
+        }
+    }
+}
+
+function isNowObi(idx1) {
+    const now = new Date();
+    var hour = now.getHours();
+    hour = Math.floor(hour / 2) + hour % 2;
+    hour = hour % 12
+    if (hour !== idx1) return false
+    return true
+}
+
+init_obimg()
+setInterval(init_obimg, 10000);
+
