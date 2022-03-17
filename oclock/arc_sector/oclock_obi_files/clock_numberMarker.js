c2fdf3b2b7ec6881e84225541e370ae5be220599
wdingsoft <wdingbox@gmail.com>
2022-03-18 03:03:40 +0800

    modified:   ../Arclock.html new file:   clock_numberMarker.js modified:   clock_obiBlinkingBackground.js modified:   css/clock_step2c_inner_clock.css new file:   css/clock_step5_number.css

---
+++ oclock/arc_sector/oclock_obi_files/clock_numberMarker.js
@@ -0,0 +1,22 @@
+
+
+
+
+function init_hour_number() {
+    var imary = document.getElementsByClassName("Hour_Number_Marker")
+    for (var i = 0; i < 12; i++) {
+        var offx = -0.4, offy = -0.8 //offset font size;
+        if (0 === i) offx -= 0.38 //adjust for 12.
+
+        var iR = 15.8  //for  circle 
+        var x = offx + iR * Math.cos(i * Math.PI / 6 - Math.PI - Math.PI / 2)
+        var y = offy + iR * Math.sin(i * Math.PI / 6 - Math.PI - Math.PI / 2)
+        console.log(x, y)
+
+        var oimg = imary[i];
+        oimg.innerHTML = (0 === i) ? 12 : i
+        oimg.style.transform = `translate(${x}rem,${y}rem)`
+
+    }
+}
+init_hour_number();
--- oclock/arc_sector/oclock_obi_files/clock_obiBlinkingBackground.js
+++ oclock/arc_sector/oclock_obi_files/clock_obiBlinkingBackground.js
@@ -1,21 +1,22 @@
 
+
 function init_obimg() {
+    const offx = 12.5, offy = 12.5 //as decorating .clock padding: 2rem;
     var imary = document.getElementsByClassName("obimg")
-    var offx = 12.5, offy = 12.5
     for (var i = 0; i < imary.length; i++) {
         var iR = 8.5  //for inner circle img position
         if (i < 6) iR = 11.3 //for outer circle img position
         var oimg = imary[i]
         var x = offx + iR * Math.cos(i * Math.PI / 3 - Math.PI / 2)
         var y = offy + iR * Math.sin(i * Math.PI / 3 - Math.PI / 2)
-        console.log(x, y)
+        //console.log(x, y)
         oimg.style.transform = `translate(${x}rem,${y}rem)`
         //oimg.style.opacity = 0.5 + 0.2 * (Math.sin(i * Math.PI / 6 - Math.PI / 2));//(i+1)/15/2
         oimg.style.opacity = 0.5
         oimg.style.border = "2px solid #000000"
 
 
-        ////determin img in the hour to flash.
+        ////determine img in the hour to flash.
         var ihour = oimg.getAttribute("ihour");
         var chour = (new Date()).getHours();
         if (chour === 0) chour = 24
@@ -31,6 +32,7 @@ function init_obimg() {
 
 
 init_obimg()
-setInterval(init_obimg, 10000);
+
+
 
 
