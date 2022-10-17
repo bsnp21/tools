c44224ad0278636c864554d720a1990ee093b406
wdingsoft <wdingbox@gmail.com>
2022-10-17 00:29:11 -0400

    English

--- Biblical_English_Vocabulary_Analysis/englizer/js/uti_englizer.js
+++ Biblical_English_Vocabulary_Analysis/englizer/js/uti_englizer.js
@@ -17,13 +17,102 @@ var uti_englizer = {
 
 
 
-    Find_root_variants: function (obj) {
+    gen_table: function (obj) {
+        var ary = Object.keys(obj), idx = 0
+        var tbs = `<table id='tab' border='1'><caption>${ary.length}</caption><tbody>`
+        for (var i = 0; i < ary.length; i++) {
+            var key = ary[i]
+            tbs += `<tr><td>${i}</td><td id='_${idx++}' class='eWord0'>${key}</td><td>`
+            for (var k = 0; k < obj[key].length; k++) {
+                tbs += `<a id='_${idx++}'  class='eWord1'>${obj[key][k]}</a><br>`
+            }
+            tbs += `</td></tr>`
+        }
+        return tbs;
+    },
+    gen_table_event: function () {
+        $(".eWord0, .eWord1").on("mouseover", function () {
+            var THIS = this
+            $("body.shiftBtn").each(function () {
+                $(THIS).toggleClass("hili")
+            })
+        })
+        ////
+
+    },
+    save_hili: function () {
+        if(!confirm("Save?")) return;
+        var idary = []
+        $(".hili").each(function () {
+            var id=$(this).attr("id")
+            idary.push(id)
+        })
+        localStorage.setItem("hili",idary.join(","))
+    },
+    load_hili: function () {
+        if(!confirm("Load?")) return;
+        var idary = localStorage.getItem("hili").split(",")
+        idary.forEach(function(id){
+            $(`#${id}`).addClass("hili")
+        })
+    },
+
+    gen_root_variants: function (obj) {
         var lis = this.sort_by_key_length(obj)
-        var str = JSON.stringify(lis, null, 4)
-        $("body").html(`<pre>${str}</pre>`)
+        //var str = JSON.stringify(lis, null, 4)
+        //$("body").html(`<pre>${str}</pre>`)
+
+
+        const Spec2 = ["ox"]
+
+        var ary = Object.keys(lis),
+            out = {};
+        for (var i = 0; i < ary.length; i++) {
+            var key = ary[i];
+            out[key] = []
+            if (key.length <= 2) {
+                if (Spec2.indexOf(key) < 0)
+                    continue;
+            }
+            //if()
+            for (var j = i + 1; j < ary.length; j++) {
+                if (ary[j].indexOf(key) === 0) {
+                    out[key].push(ary[j])
+                    ary[j] = ""
+                    continue;
+                } else {
+                    //i = j
+                    //break;
+                }
+            }
+        }
+        return out
+    },
+    Find_root_variants: function (obj) {
+        var out = this.gen_root_variants(obj)
+        var str = this.gen_table(out)
+        //var str = JSON.stringify(out, null, 4)
+        $("body").append(`${Object.keys(out).length}<br>${str}`)
+        this.gen_table_event()
 
     }
 }
 
 
+function enable_flipflap_shift() {
+    $("body").on("keydown", function (evt) {
+        if (evt.shiftKey) {
+            console.log("shift")
+            $(this).toggleClass("shiftBtn")
+        }
+    })
+}
+function save_hili() {
+    $(".eWord")
+}
+$(function () {
 
+    enable_flipflap_shift()
+
+
+})
