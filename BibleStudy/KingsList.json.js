0bcb8bfae53de32f2ed8e64676c8a49d5528a4c5
wdingsoft <wdingbox@gmail.com>
2025-03-24 00:15:48 -0400

    new file:   KingsList.json.js Kingsnew file:   KingsTable.htm

---
+++ BibleStudy/KingsList.json.js
@@ -0,0 +1,35 @@
+var KingsList = [
+    {
+        "Names(NIV)": "David",
+        "Names(CUV)": "大卫",
+        "State": "Israel",
+        "Dynasty": "2nd King",
+        "Date": "",
+        "Kinship": "son of Jessie",
+        "tribe":"Judah",
+        "Verses": "1Sa16:19",
+        "Notes": ""
+    },
+    {
+        "Names(NIV)": "Saul",
+        "Names(CUV)": "扫罗",
+        "State": "Israel",
+        "Dynasty": "1st King",
+        "Date": "",
+        "Kinship": "",
+        "Tribe":"Ben",
+        "Verses": "1Sa9:2",
+        "Notes": ""
+    },
+    {
+        "Names(NIV)": "Solomon",
+        "Names(CUV)": "所罗门",
+        "State": "Israel",
+        "Dynasty": "3rd King",
+        "Date": "",
+        "Kinship": "son of David",
+        "Tribe":"Judah",
+        "Verses": "	2Sa5:14",
+        "Notes": ""
+    },
+]
