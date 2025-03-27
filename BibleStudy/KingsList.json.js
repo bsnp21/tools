67c8323da2cffe2e2a913d3a202cf651baaf14de
root <root@ip-172-31-24-87.ec2.internal>
2025-03-27 18:20:35 +0000

    updated notes:

--- BibleStudy/KingsList.json.js
+++ BibleStudy/KingsList.json.js
@@ -445,7 +445,7 @@ var KingsList =
         "Notes": ""
     },
     "Jehoahaz": {
-        "Names(CUV)": "",
+        "Names(CUV)": "耶哥尼雅",
         "DN": "S17",
         "Span": "3mon",
         "Date": "-609",
