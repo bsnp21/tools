8b893514b07ec569998d8d3498c2081058b51fc5
root <root@ip-172-31-24-87.ec2.internal>
2025-03-30 23:09:05 +0000

    updated notes:

--- BibleStudy/KingsList.json.js
+++ BibleStudy/KingsList.json.js
@@ -469,7 +469,7 @@ var KingsList =
         "Notes": ""
     },
     "Jehoiachin": {
-        "Name(CUV)": "约雅斤",
+        "Names(CUV)": "约雅斤",
         "DN": "S19",
         "Span": "3mon",
         "Date": "-598",
