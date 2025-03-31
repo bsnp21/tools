055835965896f3e454910171e9756e209fcce7e3
wdingsoft <wdingbox@gmail.com>
2025-03-31 09:18:04 -0400

    modified:   BsnpGuestLink.js

--- BibleStudy/BsnpGuestLink.js
+++ BibleStudy/BsnpGuestLink.js
@@ -1,1 +1,2 @@
-BsnpGuestLink = "http://34.224.88.91/wdaws/bb/np/BibleStudyNotePad.htm?sip=http://34.224.88.91:7778&SSID=1743396073545guest&vision=public"
+BsnpGuestLink = "http://34.224.88.91/wdaws/bb/np/BibleStudyNotePad.htm?sip=http://34.224.88.91:7778&SSID=1743426980540guest&vision=public"
+
